ca0532b235d44838819330d773347f7e75f7a6c0
BlvckJack <ogonofidel@gmail.com>
2022-06-16 04:26:40 -0700

    Write a program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from sys import argv as args
+
+
+def commandline_args():
+    if len(args) < 2:
+        print("{} arguments.".format(0))
+    elif len(args) == 2:
+        print("{} argument:".format(len(args[1:])))
+        for count, arg in enumerate(args[1:], start=1):
+            print("{}: {}".format(count, arg))
+    else:
+        print("{} arguments:".format(len(args[1:])))
+        for count, arg in enumerate(args[1:], start=1):
+            print("{}: {}".format(count, arg))
+
+
+if __name__ == "__main__":
+    commandline_args()
